ad77485c5076a01a0596a994b1624d779b5c5237
Matt Marlow <thequantumblink@gmail.com>
2023-05-02 10:26:24 -0500

    defined ItemType class for user definitions

---
+++ src/main/java/com/quantumblink/ItemType.java
@@ -0,0 +1,44 @@
+package com.quantumblink;
+
+public class ItemType {
+    private final int Food_ID;
+    private final String Food_name;
+    private final int calories;
+    private final int fat;
+    private final int protein;
+
+    public ItemType(int food_ID, String food_name, int calories, int fat, int protein, int carbs) {
+        Food_ID = food_ID;
+        Food_name = food_name;
+        this.calories = calories;
+        this.fat = fat;
+        this.protein = protein;
+        this.carbs = carbs;
+    }
+
+    public int getFood_ID() {
+        return Food_ID;
+    }
+
+    public String getFood_name() {
+        return Food_name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    private final int carbs;
+}
